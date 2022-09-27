381fd32d5e1c70d069ff2d0c65468c6e9ce6e888
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-09-27 21:31:29 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,14 +1,14 @@
 return  {
         [""]
-		= {"SPECIAL GUY",Color3.new(0.333333, 0.333333, 1)},
+		= {"SPECIAL GUY",Color3.new(255, 8, 8)},
 	[""] 
-		= {"SPECIAL GUY",Color3.new(0.333333, 0.333333, 1)}, 
+		= {"SPECIAL GUY",Color3.new(255, 8, 8)}, 
 	[""] 
-		= {"CONFIG OWNER",Color3.new(0.333333, 1, 1)}, 
+		= {"CONFIG OWNER",Color3.new(255, 8, 8)}, 
 	[""] 
-		= {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER
+		= {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER
 	[" "] 
-	        = {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER 
-	[""]
-	        = {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER
+	        = {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER 
+	["0f83a53e75a928ce1e5c7523caf0b9d5174ed2e65e271d590d1533d0bf18bb2a83c79223c15724c36367841c18c7d52feba119728887a4a919bd4a7cacd1690c"]
+	        = {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER
 }
