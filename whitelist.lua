ab001d516e6ebe7f0bf2e5c4731bb603b519931a
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-09-27 10:50:09 +0800

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,14 @@
+return  {
+        [""]
+		= {"SPECIAL GUY",Color3.new(0.333333, 0.333333, 1)},
+	[""] 
+		= {"SPECIAL GUY",Color3.new(0.333333, 0.333333, 1)}, 
+	[""] 
+		= {"CONFIG OWNER",Color3.new(0.333333, 1, 1)}, 
+	[""] 
+		= {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER
+	[" "] 
+	        = {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER 
+	[""]
+	        = {"CONFIG OWNER 👑",Color3.new(0.666667, 0, 1)}, -- OWNER
+}
