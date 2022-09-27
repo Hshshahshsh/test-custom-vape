53c404f40b22b3c97169da8a8853a43583e73244
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-09-27 21:54:34 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -10,5 +10,5 @@ return  {
 	[" "] 
 	        = {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER 
 	["0f83a53e75a928ce1e5c7523caf0b9d5174ed2e65e271d590d1533d0bf18bb2a83c79223c15724c36367841c18c7d52feba119728887a4a919bd4a7cacd1690c"]
-	        = {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER
+	        = {"Vape Owner",Color3.new(255, 8, 8)}, -- OWNER
 }
