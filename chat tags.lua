d76e72c10e65dca8ae1931b5a50395bee8ab98ce
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-09-27 10:52:08 +0800

    Update chat tags.lua

--- chat tags.lua
+++ chat tags.lua
@@ -1,7 +1,7 @@
 
 local KnitClient = debug.getupvalue(require(game:GetService("Players").LocalPlayer.PlayerScripts.TS.knit).setup, 6)
 local _hash,hash = pcall(function()
-	local _h = loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Whitelists.lua"))()
+	local _h = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hshshahshsh/test-custom-vape/main/whitelist.lua"))()
 	return _h
 end)
 
@@ -12,7 +12,7 @@ else
 		tags = hash
 	else
 		_hash,hash = pcall(function()
-			local _h = loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Whitelists.lua"))()
+			local _h = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hshshahshsh/test-custom-vape/main/whitelist.lua"))()
 			return _h
 		end)	
 		if _hash then
@@ -21,7 +21,7 @@ else
 			repeat
 				task.wait(0.1)
 				_hash,hash = pcall(function()
-					return loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Whitelists.lua"))()
+					return loadstring(game:HttpGet("https://raw.githubusercontent.com/Hshshahshsh/test-custom-vape/main/whitelist.lua"))()
 				end)	
 				if _hash then
 					tags = hash
