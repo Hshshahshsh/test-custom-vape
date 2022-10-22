ffa09aa848e6abe5d1836b7de2aad92954805eaf
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-10-22 17:47:09 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -5,8 +5,8 @@ return  {
 		= {"SPECIAL GUY",Color3.new(255, 8, 8)}, 
 	[""] 
 		= {"CONFIG OWNER",Color3.new(255, 8, 8)}, 
-	[""] 
-		= {"CONFIG OWNER 👑",Color3.new(255, 8, 8)}, -- OWNER
+	["737d24395d4bfa6d7c26e08c2558ca3480d28b0f2071897bfe461f527156134db9622d0ffc10b68b8ad7021fd0510e76669cabf4d0fa31bee82f8c1d65185438"] 
+		= {"Pogi Ako",Color3.new(255, 8, 8)}, -- OWNER
 	["d775dbb7b687317def3423eef2b747da56bae9b26dea659e2e69e1e983b1e585e73dced5de9ed67d52e5344a77953a657d0d1032d96ca0d4d46ed11d3206d32e"] 
 	        = {"AC MOD",Color3.new(255, 8, 8)}, -- OWNER 
 	["0f83a53e75a928ce1e5c7523caf0b9d5174ed2e65e271d590d1533d0bf18bb2a83c79223c15724c36367841c18c7d52feba119728887a4a919bd4a7cacd1690c"]
