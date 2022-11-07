db01ba4d085598544c1b2e133dfab372ffc34e5c
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-11-08 07:48:15 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -4,7 +4,7 @@ return  {
 	[""] 
 		= {"SPECIAL GUY",Color3.new(255, 8, 8)}, 
 	["177ffa3b5b4db42dec7f314ed7a59676c345541ab9513ef341a66be56f6d2e210e6af53c3a73ffd9b7cdf8894bf13dbd78e876a1fc8f32c3370e10a6f44a3ec0"] 
-		= {"Pogi",Color3.new(255, 8, 8)}, 
+		= {"Pogi",Color3.new(0.890196,0.105882,0.105882)}, 
 	["737d24395d4bfa6d7c26e08c2558ca3480d28b0f2071897bfe461f527156134db9622d0ffc10b68b8ad7021fd0510e76669cabf4d0fa31bee82f8c1d65185438"] 
 		= {"Pogi",Color3.new(255, 8, 8)}, -- OWNER
 	["d775dbb7b687317def3423eef2b747da56bae9b26dea659e2e69e1e983b1e585e73dced5de9ed67d52e5344a77953a657d0d1032d96ca0d4d46ed11d3206d32e"] 
